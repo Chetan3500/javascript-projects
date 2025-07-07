161fe06f8da9bc2f28cd6eb84f2a58c3e4d7e435
Chetan Tekam <tekamchetan3500@gmail.com>
2025-07-07 10:59:59 +0530

    feat: add const variables

---
+++ 02-calorie-counter/script.js
@@ -0,0 +1,8 @@
+// document.getElementById("id-value") -> take id
+const calorieCounter = document.getElementById("calorie-counter");
+const budgetNumberInput = document.getElementById("budget");
+const entryDropdown = document.getElementById("entry-dropdown");
+const addEntryButton = document.getElementById("add-entry");
+const clearButton = document.getElementById("clear");
+const output = document.getElementById("output");
+
